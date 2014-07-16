220e8f90f680fd078f2d0739377567657f31d926
AlDrac <nikraman@gmail.com>
2014-07-16 10:14:33 +0300

    Refactoring

--- MonitorSites/public_html/core/js/scripts.js
+++ MonitorSites/public_html/core/js/scripts.js
@@ -1,28 +1,63 @@
 (function (){
     var app = angular.module('promoboxAdminApp', []);
+    
+    app.controller('userControl',[ '$http', function($http){
+        this.page = 0;
 
-    app.controller('userControl', function(){
-        this.text = lenguage;
+        this.selectLink = function(link) {
+            this.page = link;
+        };
+        
+        this.pageSelected= function(selected) {
+          return this.page === selected;  
+        };
+        
+//        var lenguage = this;
+//        $http.get('/json/en.json').seccess(function (data){
+//        lenguage.text = data;
+//        });
+    }]);
+    
+    app.directive('loginForm', function(){
+        return {
+          restrict: 'E',
+          templateUrl: 'login-form.html',
+          controller: function() {
+              this.text = lenguage.login_form;
+          },
+          controllerAs: 'login'
+        };
+    });
+    
+    app.directive('registerForm', function(){
+        return {
+          restrict: 'E',
+          templateUrl: 'register-form.html',
+          controller: function() {
+              this.text = lenguage.registration_form;
+          },
+          controllerAs: 'register'
+        };
     });
     
     var lenguage = {
-        login_form: {
-            heder: "PromoBox Login",
-            email: "Email",
-            password: "Password",
-            remember: "Remember me",
-            sign_in: "Sign In",
-            register: "Register",
+        'login_form': {
+            'heder': "PromoBox Login",
+            'email': "Email",
+            'password': "Password",
+            'remember': "Remember me",
+            'sign_in': "Sign In",
+            'register': "Register"
         },
-        registration_form: {
-            heder: "PromoBox Registration",
-            firstname: "Firstname",
-            lastname: "Lastname",
-            company: "Company",
-            email: "Email",
-            remember: "Remember me",
-            register: "Register",
-            sign_in: "Sign In",
+        'registration_form': {
+            'heder': "PromoBox Registration",
+            'firstname': "Firstname",
+            'lastname': "Lastname",
+            'company': "Company",
+            'email': "Email",
+            'remember': "Remember me",
+            'register': "Register",
+            'sign_in': "Sign In"
         }
     };
     

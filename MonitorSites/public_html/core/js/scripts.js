ac126dbb44ee6de894694e3901cf89775cb7188d
AlDrac <nikraman@gmail.com>
2014-07-18 00:12:15 +0300

    Complete Login

--- MonitorSites/public_html/core/js/scripts.js
+++ MonitorSites/public_html/core/js/scripts.js
@@ -2,11 +2,10 @@
     var app = angular.module('promoboxAdminApp', []);
     
     app.controller('userControl',[ '$http','$scope', function($http, $scope){
-        $scope.page = this.page;
         $scope.page = 0;
-        
+        this.title = 'PromoBox';
 
-        this.selectLink = function(link) {
+        $scope.selectLink = function(link) {
             $scope.page = link;
         };
         
@@ -21,12 +20,32 @@
           restrict: 'E',
           templateUrl: 'login-form.html',
           controller: function($http,$scope) {
-              var language = this;
+              var login = this;
+              
               $http.get("http://localhost:8383/MonitorSites/core/json/en.json").success(function (s_data) {
-                  language.text = s_data.login_form;
+                  login.text = s_data.login_form;
               }).error(function (e_data){
                   alert('Error');
               });
+              
+              $scope.submitLogin = function () {
+                if ($scope.loginForm.$valid) {
+                    var email = $scope.loginForm.email.$modelValue;
+                    var password = $scope.loginForm.password.$modelValue;
+                    var remember = $scope.loginForm.remember.$modelValue;
+                    
+                    $http.get("http://localhost:8383/MonitorSites/core/json/user_data.json").success(function (s_data) {
+                        if(s_data.return) {
+                            $scope.id = s_data.id;
+                            $scope.login = s_data.login;
+                            
+                            $scope.selectLink(2);
+                        }
+                    }).error(function (e_data){
+                        alert('Error');
+                    });
+		}
+              };
           },
           controllerAs: 'login'
         };
@@ -37,14 +56,26 @@
           restrict: 'E',
           templateUrl: 'register-form.html',
           controller: function($http,$scope) {
-              var language = this;
+              var register = this;
+              
               $http.get("http://localhost:8383/MonitorSites/core/json/en.json").success(function (s_data) {
-                  language.text = s_data.registration_form;
+                  register.text = s_data.registration_form;
               }).error(function (e_data){
                   alert('Error');
               });
           },
           controllerAs: 'register'
+        };
+    });
+    
+    app.directive('mainpage', function(){
+        return {
+          restrict: 'E',
+          templateUrl: 'mainpage.html',
+          controller: function($http,$scope) {
+              
+          },
+          controllerAs: 'mainpage'
         };
     });
     

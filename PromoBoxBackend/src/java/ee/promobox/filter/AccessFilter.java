f3d1804957ddf45b8ff8de291bde03bb4423636a
Maxim Dorofeev <maxim@javelin>
2014-07-22 17:34:34 +0300

    Added access filter

---
+++ PromoBoxBackend/src/java/ee/promobox/filter/AccessFilter.java
@@ -0,0 +1,53 @@
+package ee.promobox.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class AccessFilter implements Filter {
+
+    private FilterConfig filterConfig = null;
+
+    public AccessFilter() {
+    	
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletResponse httpServletResponse = ((HttpServletResponse) response);
+
+        request.setCharacterEncoding("UTF-8");
+
+        httpServletResponse.setCharacterEncoding("UTF-8");
+
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        
+        chain.doFilter(request, response);
+
+    }
+
+    public FilterConfig getFilterConfig() {
+        return (this.filterConfig);
+    }
+
+    public void setFilterConfig(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+    }
+
+    public void destroy() {
+    }
+
+    public void init(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+    }
+}

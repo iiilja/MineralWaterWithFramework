a21eba699dd1f3c2ae18465a0cefc4ecc5c214d8
AlDrac <nikraman@gmail.com>
2014-07-27 21:35:06 +0300

    Add Angular Upload

--- PromoboxFrontend/public/js/app.js
+++ PromoboxFrontend/public/js/app.js
@@ -1,6 +1,6 @@
 var apiEndpoint = "http://api.dev.promobox.ee/service/";
 
-var app = angular.module('promobox', ['ngRoute', 'ui.bootstrap', 'pascalprecht.translate', 'promobox.services']);
+var app = angular.module('promobox', ['ngRoute', 'ui.bootstrap', 'pascalprecht.translate', 'promobox.services', 'angularFileUpload']);
 
 
 app.config(['$routeProvider', function ($routeProvider) {
@@ -86,14 +86,47 @@ app.controller('RegistrationController', ['$scope', '$location', '$http', 'token
         }
     }]);
 
-app.controller('CampaignEditController', ['$scope', '$routeParams', 'token', 'Campaign',
-    function ($scope, $routeParams, token, Campaign) {
+app.controller('CampaignEditController', ['$scope', '$routeParams', 'token', 'Campaign', '$upload',
+    function ($scope, $routeParams, token, Campaign, $upload) {
        if (token.check()) {
            $scope.campaign = Campaign.get({id: $routeParams.cId, token: token.get()});
            console.log($scope.campaign);
 
            $scope.edit_company = function () {
                console.log($scope.edit_company_form);
+           };
+
+           $scope.onFileSelect = function($files) {
+               //$files: an array of files selected, each file has name, size, and type.
+               for (var i = 0; i < $files.length; i++) {
+                   var file = $files[i];
+                   $scope.upload = $upload.upload({
+                       url: 'upload', //upload.php script, node.js route, or servlet url
+                       method: 'POST',
+                       //headers: {'header-key': 'header-value'},
+                       //withCredentials: true,
+                       //data: {myObj: $scope.myModelObj},
+                       file: file // or list of files ($files) for html5 only
+                       //fileName: 'doc.jpg' or ['1.jpg', '2.jpg', ...] // to modify the name of the file(s)
+                       // customize file formData name ('Content-Desposition'), server side file variable name.
+                       //fileFormDataName: myFile, //or a list of names for multiple files (html5). Default is 'file'
+                       // customize how data is added to formData. See #40#issuecomment-28612000 for sample code
+                       //formDataAppender: function(formData, key, val){}
+                   }).progress(function(evt) {
+                       console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+                   }).success(function(data, status, headers, config) {
+                       // file is uploaded successfully
+                       console.log(data);
+                   });
+                   //.error(...)
+                   //.then(success, error, progress);
+                   // access or attach event listeners to the underlying XMLHttpRequest.
+                   //.xhr(function(xhr){xhr.upload.addEventListener(...)})
+               }
+               /* alternative way of uploading, send the file binary with the file's content-type.
+                Could be used to upload files to CouchDB, imgur, etc... html5 FileReader is needed.
+                It could also be used to monitor the progress of a normal http post/put request with large data*/
+               // $scope.upload = $upload.http({...})  see 88#issuecomment-31366487 for sample code.
            };
 
        }

a3606bbaa5a98a73ca429560857323be44469199
Maxim Dorofeev <mdorofeev@gmail.com>
2014-08-18 20:58:04 +0300

    Small fix

--- PromoBoxAndroid/app/src/main/java/ee/promobox/promoboxandroid/ImageActivity.java
+++ PromoBoxAndroid/app/src/main/java/ee/promobox/promoboxandroid/ImageActivity.java
@@ -52,10 +52,7 @@ public class ImageActivity extends Activity {
     private void hideSystemUI() {
 
         this.getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
                         | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
         );
     }

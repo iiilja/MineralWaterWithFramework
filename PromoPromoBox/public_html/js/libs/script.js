6c0aafc2091c7c06450ad969ee2ad3dfd928ab12
AlDrac <nikraman@gmail.com>
2014-09-15 03:06:41 +0300

    Add Template Promobox
    Price have BUG

---
+++ PromoPromoBox/public_html/js/libs/script.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+//    (function animate() {
+//        var img = 1;
+//        var block = $('html');
+//        block.animate({backgroundColor: $.Color(block.css('backgroundColor')).hue('+=179')}, 3000, animate);
+//    })();
+});

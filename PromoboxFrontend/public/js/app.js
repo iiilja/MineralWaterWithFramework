b24a67c59f8acfc2d36b00d2a2c65e28cce20c1f
Vitali Spiridonov <vitali.spiridonov@gmail.com>
2015-01-12 16:04:33 +0200

    Refacotring #17

--- PromoboxFrontend/public/js/app.js
+++ PromoboxFrontend/public/js/app.js
@@ -271,32 +271,11 @@ app.controller('CampaignEditController', ['$scope', '$stateParams', 'token', 'Ca
             }
         };
         
-        
 
-        $scope.workhours = ['0', 
-                            '1',
-                            '2',
-                            '3',
-                            '4',
-                            '5',
-                            '6',
-                            '7',
-                            '8',
-                            '9',
-                            '10',
-                            '11',
-                            '12',
-                            '13',
-                            '14',
-                            '15',
-                            '16',
-                            '17',
-                            '18',
-                            '19',
-                            '20',
-                            '21',
-                            '22',
-                            '23'];
+        $scope.workhours = [];
+        for (var i = 0; i < 24; i++) {
+              $scope.workhours.push(i + "");
+        }
         
         $scope.addWorkhours = [];
         $scope.removeWorkhours = [];
